0b204c756b2e2d8107bd0c6204514dce8d9bf855
jhap1982 <jhap1982@gmail.com>
2019-08-09 17:27:55 -0300

    #1 Committing some stuff to see what happens using github issues

---
+++ hello-world-java-app/src/it/java/com/jhap1982/helloworld/MainAppIntegTest.java
@@ -0,0 +1,17 @@
+package com.jhap1982.helloworld;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * MainApp integration test class.
+ * 
+ * @author jhap1982
+ */
+public class MainAppIntegTest {
+
+	@Test
+	public void testMainApp() {
+		// Nothing to do, just dummy test
+	}
+	
+}
