954211511df2fdb57120933a8ca5bdfec5ac0fa3
Juan Achucarro <jhap1982@gmail.com>
2020-06-13 03:07:17 -0300

    IT Test folder fixes

---
+++ hello-world-ejb-stateful/src/it-test/java/com/jhap1982/helloworld/ejb/stateful/HelloWorldEJBStatefulIntegTest.java
@@ -0,0 +1,17 @@
+package com.jhap1982.helloworld.ejb.stateful;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * HelloWorldEJBStateful integration test class.
+ * 
+ * @author jhap1982
+ */
+public class HelloWorldEJBStatefulIntegTest {
+
+	@Test
+	public void testHelloWorldEJBStatefulHelloWorld() {
+		// Nothing to do yet
+	}
+	
+}
