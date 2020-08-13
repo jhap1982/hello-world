2b5c65e8d9e8bbb86dfc25c5ff11460592b56b38
jhap1982 <jhap1982@gmail.com>
2020-08-12 23:30:49 -0300

    Hello world of libgdx (1.9.11) desktop application.

---
+++ hello-world-libgdx-desktop/desktop/src/com/jhap/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jhap.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jhap.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
