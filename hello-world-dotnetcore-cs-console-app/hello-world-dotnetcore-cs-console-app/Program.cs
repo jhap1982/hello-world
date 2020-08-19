f074bf67897ac101284d837978954a64a701b740
jhap1982 <jhap1982@gmail.com>
2020-08-19 11:07:34 -0300

    Hello World .NET Core console application.

---
+++ hello-world-dotnetcore-cs-console-app/hello-world-dotnetcore-cs-console-app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world_dotnetcore_cs_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
