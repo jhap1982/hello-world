41c7a8c0ba836f6a778cfb670bcb1ee786b34367
Admin <Admin@DESKTOP-U71384H>
2021-08-15 15:26:01 -0300

    Hello world solution for ASP dot net core (c#) using ReactJS only.

---
+++ hello-world-asp-dotnetcore-csharp-reactjs/hello-world-asp-dotnetcore-csharp-reactjs/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace hello_world_asp_dotnetcore_csharp_reactjs
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
