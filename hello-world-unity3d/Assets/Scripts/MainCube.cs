df1485fc89e3f68a38b78bbd7d8cab1614e9ee13
jhap1982 <jhap1982@gmail.com>
2021-08-20 20:45:42 -0300

    Update project.

--- hello-world-unity3d/Assets/Scripts/MainCube.cs
+++ hello-world-unity3d/Assets/Scripts/MainCube.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class MainCube : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world!");
+        Debug.Log("Hello world from cube!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Nothing to do
+        
     }
 }
