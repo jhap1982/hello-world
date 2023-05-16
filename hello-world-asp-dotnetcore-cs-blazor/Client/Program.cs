b9b90f670a0ab1189c75957d509e375bc473edf9
jhap1982 <jhap1982@gmail.com>
2023-05-16 00:09:16 -0300

    Add resources

---
+++ hello-world-asp-dotnetcore-cs-blazor/Client/Program.cs
@@ -0,0 +1,11 @@
+using hello_world_asp_dotnetcore_cs_blazor.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
